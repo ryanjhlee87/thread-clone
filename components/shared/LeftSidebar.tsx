1d791d9c56f4812bcb57ea02d7de9a06c3043a0c
ryanjhlee87 <108978592+ryanjhlee87@users.noreply.github.com>
2023-10-07 17:56:07 +0900

    Add notification

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,34 +1,33 @@
 'use client';
 
-import { sidebarLinks } from '@/constants';
-import { SignOutButton, SignedIn, useAuth } from '@clerk/nextjs';
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname, useRouter } from 'next/navigation';
-import { Router } from 'next/router';
-import React from 'react';
+import { SignOutButton, SignedIn, useAuth } from '@clerk/nextjs';
+
+import { sidebarLinks } from '@/constants';
 
 const LeftSidebar = () => {
   const router = useRouter();
+  const pathname = usePathname();
+
   const { userId } = useAuth();
+
   return (
-    <section className="custom-scrolbar leftsidebar">
+    <section className="custom-scrollbar leftsidebar">
       <div className="flex w-full flex-1 flex-col gap-6 px-6">
         {sidebarLinks.map(link => {
-          const pathname = usePathname();
-
           const isActive =
             (pathname.includes(link.route) && link.route.length > 1) ||
             pathname === link.route;
 
-          if (link.route === '/profile')
-            link.route = `${link.route}/${userId}}`;
+          if (link.route === '/profile') link.route = `${link.route}/${userId}`;
 
           return (
             <Link
               href={link.route}
               key={link.label}
-              className={`${isActive && 'bg-primary-500'} leftsidebar_link`}
+              className={`leftsidebar_link ${isActive && 'bg-primary-500 '}`}
             >
               <Image
                 src={link.imgURL}
@@ -36,16 +35,17 @@ const LeftSidebar = () => {
                 width={24}
                 height={24}
               />
+
               <p className="text-light-1 max-lg:hidden">{link.label}</p>
             </Link>
           );
         })}
       </div>
 
-      <div className="mt-10 px-8">
+      <div className="mt-10 px-6">
         <SignedIn>
           <SignOutButton signOutCallback={() => router.push('/sign-in')}>
-            <div className="flex cursor-pointer">
+            <div className="flex cursor-pointer gap-4 p-4">
               <Image
                 src="/assets/logout.svg"
                 alt="logout"
@@ -53,7 +53,7 @@ const LeftSidebar = () => {
                 height={24}
               />
 
-              <p className="text-light-2 mx-lg:hidden gap-4 p-4">Logout</p>
+              <p className="text-light-2 max-lg:hidden">Logout</p>
             </div>
           </SignOutButton>
         </SignedIn>
